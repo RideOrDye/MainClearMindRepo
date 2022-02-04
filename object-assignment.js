43f57816194076e612234d42a8e4c9760d360025
RideOrDye <63518641+RideOrDye@users.noreply.github.com>
2022-02-03 22:47:34 -0800

    add folders

--- object-assignment.js
+++ object-assignment.js
@@ -1,52 +1,31 @@
-/*
-
-Below is how the final product should look like. 
-The assignment is to console log the folder name and contents name. Since its an array of objects im having
-trouble accessing nested objects. 
-
-- Work 
-    + Code
-    + Meetings
-    + Notes
-- Personal
-    + My Music
-    + My Youtube Videos
-    + Random Links
-- Purchases
-    + Amazon
-    + Zappos
-    + Petco
-
-*/
-
 
 
 const folders = [
     {
         name: 'Work',
         contents: [
-        {
-            name: 'Code',
-            contents: [],
-            dateCreated: '11/22/2021',
-            id: 387,
-            level: 2
-        },
-        {
-            name: 'Meetings',
-            contents: [],
-            dateCreated: '11/22/2021',
-            id: 889,
-            level: 2
-        },
-        {
-            name: 'Notes',
-            contents: [],
-            dateCreated: '11/22/2021',
-            id: 3,
-            level: 2
-        }
-        ], 
+            {
+                name: 'Code',
+                contents: [],
+                dateCreated: '11/22/2021',
+                id: 387,
+                level: 2
+            },
+            {
+                name: 'Meetings',
+                contents: [],
+                dateCreated: '11/22/2021',
+                id: 889,
+                level: 2
+            },
+            {
+                name: 'Notes',
+                contents: [],
+                dateCreated: '11/22/2021',
+                id: 3,
+                level: 2
+            }
+        ],
         dateCreated: '11/22/2021',
         id: 1,
         level: 1
@@ -54,28 +33,28 @@ const folders = [
     {
         name: 'Personal',
         contents: [
-        {
-            name: 'My Music',
-            contents: [],
-            dateCreated: '11/22/2021',
-            id: 4,
-            level: 2
-        },
-        {
-            name: 'My Youtube Videos',
-            contents: [],
-            dateCreated: '11/22/2021',
-            id: 9,
-            level: 2
-        },
-        {
-            name: 'Random Links',
-            contents: [],
-            dateCreated: '11/22/2021',
-            id: 7,
-            level: 2
-        }
-    ],
+            {
+                name: 'My Music',
+                contents: [],
+                dateCreated: '11/22/2021',
+                id: 4,
+                level: 2
+            },
+            {
+                name: 'My Youtube Videos',
+                contents: [],
+                dateCreated: '11/22/2021',
+                id: 9,
+                level: 2
+            },
+            {
+                name: 'Random Links',
+                contents: [],
+                dateCreated: '11/22/2021',
+                id: 7,
+                level: 2
+            }
+        ],
         dateCreated: '11/22/2021',
         id: 13,
         level: 1
@@ -83,28 +62,28 @@ const folders = [
     {
         name: 'Purchases',
         contents: [
-        {
-            name: 'Amazon',
-            contents: [],
-            dateCreated: '11/22/2021',
-            id: 90,
-            level: 2
-        },
-        {
-            name: 'Zappos',
-            contents: [],
-            dateCreated: '11/22/2021',
-            id: 91,
-            level: 2
-        },
-        {
-            name: 'Petco',
-            contents: [],
-            dateCreated: '11/22/2021',
-            id: 79,
-            level: 2
-        }
-    ],
+            {
+                name: 'Amazon',
+                contents: [],
+                dateCreated: '11/22/2021',
+                id: 90,
+                level: 2
+            },
+            {
+                name: 'Zappos',
+                contents: [],
+                dateCreated: '11/22/2021',
+                id: 91,
+                level: 2
+            },
+            {
+                name: 'Petco',
+                contents: [],
+                dateCreated: '11/22/2021',
+                id: 79,
+                level: 2
+            }
+        ],
         dateCreated: '11/22/2021',
         id: 12,
         level: 1
@@ -113,19 +92,9 @@ const folders = [
 
 
 
-folders.forEach(function (arrayItem) {
-    var x = arrayItem.name;
-    console.log(x);
-});
-
-for (let i = 0; i < folders.contents.name.length; i++) {
-    let y = folders.contents[name];
-    console.log(y)
-    
- 		}
-
-         //adding this comment to show as change
-
-         //try again
-
-const change = 1
+for (let i = 0; i < folders.length; i++) {
+    console.log("-", folders[i].name);
+    for (let j = 0; j < folders[i].contents.length; j++) {
+        console.log("    +", folders[i].contents[j].name);
+    }
+}
